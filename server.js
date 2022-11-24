5488897a1b6cd25c5e755cb029ad15a853b4935f
Rahul <rahulkarda2002@gmail.com>
2022-11-24 16:46:29 +0530

    updated css

--- server.js
+++ server.js
@@ -1,25 +1,21 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const { response } = require('express')
 const PORT = 8000
 
 app.use(cors())
-
 
 app.get('/', (request, response)=>{
     response.sendFile(__dirname + '/index.html')
 })
 
-// app.get('/api/:name',(request,response)=>{
-//     const rapperName = request.params.name.toLowerCase()
+app.get('/autism'),(request,response)=>{
+    response.sendFile(__dirname + '/autism.html')
+}
 
-//     if( rappers[rapperName] ){
-//         response.json(rappers[rapperName])
-//     }else{
-//         response.json(rappers['unknown'])
-//     }
-    
-// })
+
+
 
 app.listen(process.env.PORT || PORT, ()=>{
     console.log(`The server is now running on port ${PORT}! Betta Go Catch It!`)
